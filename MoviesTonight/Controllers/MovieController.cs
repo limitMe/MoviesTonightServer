f4ae5602521b4a38d3587f4964ab720213f843e6
钟典@Runga <ceo@myaiol.com>
2017-08-19 19:54:42 +1000

    Added the Model

---
+++ MoviesTonight/Controllers/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using MoviesTonight.Models;
+
+namespace MoviesTonight.Controllers
+{
+    [Route("api/[controller]")]
+    public class MovieController : Controller
+    {
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public MovieInfo Get(int id)
+        {
+            var aMovie = new MovieInfo();
+            aMovie.movieName = "asda";
+            aMovie.discription = "sdadad";
+            aMovie.pictureUrl = "asdada";
+            return aMovie;
+        }
+
+    }
+}
---
+++ MoviesTonight/Models/MovieInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MoviesTonight.Models
+{
+    public class MovieInfo
+    {
+        public string movieName { get; set; }
+        public string discription { get; set; }
+        public string pictureUrl { get; set; }
+    }
+}
